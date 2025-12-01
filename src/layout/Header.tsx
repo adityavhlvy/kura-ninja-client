6e1a72d122817435a1cc81e6c9c678dd7a42de38
Aditya Vahlevy Nugraha <adityavahlevy1003@gmail.com>
2025-12-01 15:08:34 +0700

    fix: change header for phone layout

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -47,10 +47,10 @@ export default function Header({ isSidebarOpen, toggleSidebar }: HeaderProps) {
                 </button>
                 <Link to="/" className="btn btn-ghost text-xl">Logo</Link>
             </div>
-            <div className='navbar-center hidden md:flex'>
-                <div className="flex items-center gap-2 px-3 py-1 rounded-md bg-base-200 text-base-content text-sm font-mono border border-base-300">
-                    <SlDoc size={16} />
-                    <span className="font-bold">{getBreadcrumbs(location.pathname)}</span>
+            <div className='navbar-center'>
+                <div className="flex items-center gap-2 px-3 py-1 rounded-md bg-base-200 text-base-content text-xs md:text-sm font-mono border border-base-300">
+                    <SlDoc size={16} className="shrink-0" />
+                    <span className="font-bold truncate max-w-[150px] md:max-w-none">{getBreadcrumbs(location.pathname)}</span>
                 </div>
             </div>
             <div className='navbar-end'>
