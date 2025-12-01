6e771dc657a0031be491f480eea184ae2e87277c
Aditya Vahlevy Nugraha <adityavahlevy1003@gmail.com>
2025-12-01 09:58:10 +0700

    feat(components): create reusable SEO component with helmet support

---
+++ src/components/SEO.tsx
@@ -0,0 +1,41 @@
+import { Helmet } from 'react-helmet-async';
+
+interface SEOProps {
+    title: string;
+    description: string;
+    name?: string;
+    type?: string;
+    image?: string;
+    url?: string;
+}
+
+export default function SEO({
+    title,
+    description,
+    name = 'Aditya Vahlevy Nugraha',
+    type = 'website',
+    image = 'https://kura-ninja.com/og-image.jpg',
+    url = 'https://kura-ninja.com/',
+}: SEOProps) {
+    return (
+        <Helmet>
+            {/* Standard metadata tags */}
+            <title>{title}</title>
+            <meta name='description' content={description} />
+
+            {/* Facebook tags */}
+            <meta property='og:type' content={type} />
+            <meta property='og:title' content={title} />
+            <meta property='og:description' content={description} />
+            <meta property='og:image' content={image} />
+            <meta property='og:url' content={url} />
+
+            {/* Twitter tags */}
+            <meta name='twitter:creator' content={name} />
+            <meta name='twitter:card' content={type === 'article' ? 'summary_large_image' : 'summary'} />
+            <meta name='twitter:title' content={title} />
+            <meta name='twitter:description' content={description} />
+            <meta name='twitter:image' content={image} />
+        </Helmet>
+    );
+}
