d2b0fa6abc0f6165d76e64be247318166849d931
Aditya Vahlevy Nugraha <adityavahlevy1003@gmail.com>
2025-12-09 14:21:04 +0700

    chore(build): implement manual code splitting to optimize production bundle size

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,26 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 6969,
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react-icons')) {
+              return 'icons';
+            }
+            if (id.includes('framer-motion')) {
+              return 'animations';
+            }
+            if (id.includes('react-hot-toast') || id.includes('react-helmet-async')) {
+              return 'ui';
+            }
+            if (id.includes('react') || id.includes('react-dom') || id.includes('react-router-dom')) {
+              return 'vendor';
+            }
+          }
+        },
+      },
+    },
+  },
 })
