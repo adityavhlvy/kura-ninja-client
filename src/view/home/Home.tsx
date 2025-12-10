2b9b4a6c46423b4d8b69408f52e37a3a05ff28a9
Aditya Vahlevy Nugraha <adityavahlevy1003@gmail.com>
2025-12-10 16:27:08 +0700

    chore(content): update home page seo keywords

--- src/view/home/Home.tsx
+++ src/view/home/Home.tsx
@@ -36,7 +36,7 @@ export default function Home() {
             <SEO
                 title="Aditya Vahlevy Nugraha - Fullstack Developer"
                 description="Aditya Vahlevy Nugraha is a Fullstack Developer, Software Engineer, and Data Science Enthusiast based in Indonesia. Explore his projects and learn more about his work."
-                keywords="Aditya Vahlevy Nugraha, Fullstack Developer, Software Engineer, Data Science, Indonesia, React, Node.js, Python, Portfolio"
+                keywords="Aditya Vahlevy Nugraha, Fullstack Developer, Software Engineer, Data Science, Project Manager, Project Management, Beasiswa APERTI BUMN 2021, Jakarta, Dumai, Riau, Indonesia, SMAN PLUS Provinsi Riau, Golang, Go Fiber, Fiber, React, Next.js, Python, Portfolio"
             />
 
             {/* Global Background Effects */}
